f33dc17055835838cdbeaf1700ccd0b8658fb86c
Mohamed Ibrahim <mo3dev@gmail.com>
2020-10-06 09:25:31 -0400

    Updated the oscar repository URL to clone

--- clone.sh
+++ clone.sh
@@ -3,7 +3,7 @@ echo "Cloning oscar"
 if [ -d "./oscar" ]; then
     echo "already cloned"
 else
-    git clone --depth 1 --single-branch --branch docker-master --config core.autocrlf=input git@github.com:mo3dev/oscar.git oscar
+    git clone --depth 1 --single-branch --branch docker-master --config core.autocrlf=input git@github.com:hamilton-health-sciences/oscar.git oscar
 fi
 
 
